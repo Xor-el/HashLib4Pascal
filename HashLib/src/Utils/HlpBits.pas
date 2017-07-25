2f6787a61c4a386a372377895d3457258aac7442
Ugochukwu Mmaduekwe <ugo4brain@gmail.com>
2017-07-25 19:56:38 +0100

    some little modifications.

--- HashLib/src/Utils/HlpBits.pas
+++ HashLib/src/Utils/HlpBits.pas
@@ -9,11 +9,11 @@ type
 
   public
 
-    class function ReverseBytesInt32(value: Int32): Int32; static; inline;
-    class function ReverseBitsUInt8(value: UInt8): UInt8; static; inline;
-    class function ReverseBytesUInt16(value: UInt16): UInt16; static; inline;
-    class function ReverseBytesUInt32(value: UInt32): UInt32; static; inline;
-    class function ReverseBytesUInt64(value: UInt64): UInt64; static; inline;
+    class function ReverseBytesInt32(Value: Int32): Int32; static; inline;
+    class function ReverseBitsUInt8(Value: UInt8): UInt8; static; inline;
+    class function ReverseBytesUInt16(Value: UInt16): UInt16; static; inline;
+    class function ReverseBytesUInt32(Value: UInt32): UInt32; static; inline;
+    class function ReverseBytesUInt64(Value: UInt64): UInt64; static; inline;
 
     /// <summary>
     /// Reverse a ByteArray.
@@ -24,7 +24,7 @@ type
     /// <param name="Size">Size of the Array to Reverse.</param>
 
     class procedure ReverseByteArray(Source, Dest: Pointer;
-      Size: Int64); static;
+      Size: int64); static;
 
     /// <summary>
     /// Calculates Arithmetic shift right.
@@ -33,7 +33,7 @@ type
     /// <param name="ShiftBits">Integer, number of bits to shift value to.</param>
     /// <returns>Shifted value.</returns>
 
-    class function Asr32(value: Int32; ShiftBits: Int32): Int32; static; inline;
+    class function Asr32(Value: Int32; ShiftBits: Int32): Int32; static; inline;
 
     /// <summary>
     /// Calculates Arithmetic shift right.
@@ -43,7 +43,7 @@ type
     /// <returns>Shifted value.</returns>
     /// Implementation was found here <see cref="https://github.com/Spelt/ZXing.Delphi/blob/master/Lib/Classes/Common/MathUtils.pas" />
 
-    class function Asr64(value: Int64; ShiftBits: Int32): Int64; static; inline;
+    class function Asr64(Value: int64; ShiftBits: Int32): int64; static; inline;
 
     class function RotateLeft32(a_value: UInt32; a_n: Int32): UInt32; overload;
       static; inline;
@@ -60,7 +60,7 @@ implementation
 
 { TBits }
 
-class procedure TBits.ReverseByteArray(Source, Dest: Pointer; Size: Int64);
+class procedure TBits.ReverseByteArray(Source, Dest: Pointer; Size: int64);
 var
   ptr_src, ptr_dest: PByte;
 begin
@@ -76,91 +76,101 @@ begin
   end;
 end;
 
-class function TBits.ReverseBytesInt32(value: Int32): Int32;
+class function TBits.ReverseBytesInt32(Value: Int32): Int32;
 {$IFNDEF FPC}
 var
   i1, i2, i3, i4: Int32;
 {$ENDIF FPC}
 begin
 {$IFDEF FPC}
-  result := SwapEndian(value);
+  Result := SwapEndian(Value);
 {$ELSE}
-  i1 := value and $FF;
-  i2 := TBits.Asr32(value, 8) and $FF;
-  i3 := TBits.Asr32(value, 16) and $FF;
-  i4 := TBits.Asr32(value, 24) and $FF;
+  i1 := Value and $FF;
+  i2 := TBits.Asr32(Value, 8) and $FF;
+  i3 := TBits.Asr32(Value, 16) and $FF;
+  i4 := TBits.Asr32(Value, 24) and $FF;
 
-  result := (i1 shl 24) or (i2 shl 16) or (i3 shl 8) or (i4 shl 0);
+  Result := (i1 shl 24) or (i2 shl 16) or (i3 shl 8) or (i4 shl 0);
 {$ENDIF FPC}
 end;
 
-class function TBits.ReverseBitsUInt8(value: UInt8): UInt8;
+class function TBits.ReverseBitsUInt8(Value: UInt8): UInt8;
 begin
-  value := ((value shr 1) and $55) or ((value shl 1) and $AA);
-  value := ((value shr 2) and $33) or ((value shl 2) and $CC);
-  value := ((value shr 4) and $0F) or ((value shl 4) and $F0);
-  result := value;
+  Value := ((Value shr 1) and $55) or ((Value shl 1) and $AA);
+  Value := ((Value shr 2) and $33) or ((Value shl 2) and $CC);
+  Value := ((Value shr 4) and $0F) or ((Value shl 4) and $F0);
+  Result := Value;
 end;
 
-class function TBits.ReverseBytesUInt16(value: UInt16): UInt16;
+class function TBits.ReverseBytesUInt16(Value: UInt16): UInt16;
 begin
 {$IFDEF FPC}
-  result := SwapEndian(value);
+  Result := SwapEndian(Value);
 {$ELSE}
-  result := UInt16((value and UInt32($FF)) shl 8 or
-    (value and UInt32($FF00)) shr 8);
+  Result := UInt16((Value and UInt32($FF)) shl 8 or
+    (Value and UInt32($FF00)) shr 8);
 {$ENDIF FPC}
 end;
 
-class function TBits.ReverseBytesUInt32(value: UInt32): UInt32;
+class function TBits.ReverseBytesUInt32(Value: UInt32): UInt32;
 begin
 {$IFDEF FPC}
-  result := SwapEndian(value);
+  Result := SwapEndian(Value);
 {$ELSE}
-  result := (value and UInt32($000000FF)) shl 24 or (value and UInt32($0000FF00)
-    ) shl 8 or (value and UInt32($00FF0000)) shr 8 or
-    (value and UInt32($FF000000)) shr 24;
+  Result := (Value and UInt32($000000FF)) shl 24 or (Value and UInt32($0000FF00)
+    ) shl 8 or (Value and UInt32($00FF0000)) shr 8 or
+    (Value and UInt32($FF000000)) shr 24;
 {$ENDIF FPC}
 end;
 
-class function TBits.ReverseBytesUInt64(value: UInt64): UInt64;
+class function TBits.ReverseBytesUInt64(Value: UInt64): UInt64;
 begin
 {$IFDEF FPC}
-  result := SwapEndian(value);
+  Result := SwapEndian(Value);
 {$ELSE}
-  result := (value and UInt64($00000000000000FF)) shl 56 or
-    (value and UInt64($000000000000FF00)) shl 40 or
-    (value and UInt64($0000000000FF0000)) shl 24 or
-    (value and UInt64($00000000FF000000)) shl 8 or
-    (value and UInt64($000000FF00000000)) shr 8 or
-    (value and UInt64($0000FF0000000000)) shr 24 or
-    (value and UInt64($00FF000000000000)) shr 40 or
-    (value and UInt64($FF00000000000000)) shr 56;
+  Result := (Value and UInt64($00000000000000FF)) shl 56 or
+    (Value and UInt64($000000000000FF00)) shl 40 or
+    (Value and UInt64($0000000000FF0000)) shl 24 or
+    (Value and UInt64($00000000FF000000)) shl 8 or
+    (Value and UInt64($000000FF00000000)) shr 8 or
+    (Value and UInt64($0000FF0000000000)) shr 24 or
+    (Value and UInt64($00FF000000000000)) shr 40 or
+    (Value and UInt64($FF00000000000000)) shr 56;
 {$ENDIF FPC}
 end;
 
-class function TBits.Asr32(value: Int32; ShiftBits: Int32): Int32;
+class function TBits.Asr32(Value: Int32; ShiftBits: Int32): Int32;
 
 begin
 {$IFDEF FPC}
-  result := SarLongInt(value, ShiftBits);
+  Result := SarLongInt(Value, ShiftBits);
 {$ELSE}
-  result := value shr ShiftBits;
-  if (value and $80000000) > 0 then
+  Result := Value shr ShiftBits;
+  if (Value and $80000000) > 0 then
     // if you don't want to cast ($FFFFFFFF) to an Int32,
     // simply replace it with (-1) to avoid range check error.
-    result := result or (Int32($FFFFFFFF) shl (32 - ShiftBits));
+    Result := Result or (Int32($FFFFFFFF) shl (32 - ShiftBits));
+
+  /// ++++++ Alternative Variant ++++++ ///
+
+  // Result := (Value shr ShiftBits) or ((0 - ((Value shr 31) and 1)) shl
+  // (32 - ShiftBits));
 {$ENDIF FPC}
 end;
 
-class function TBits.Asr64(value: Int64; ShiftBits: Int32): Int64;
+class function TBits.Asr64(Value: int64; ShiftBits: Int32): int64;
 begin
 {$IFDEF FPC}
-  result := SarInt64(value, ShiftBits);
+  Result := SarInt64(Value, ShiftBits);
 {$ELSE}
-  result := value shr ShiftBits;
-  if (value and $8000000000000000) > 0 then
-    result := result or ($FFFFFFFFFFFFFFFF shl (64 - ShiftBits));
+  Result := Value shr ShiftBits;
+  if (Value and $8000000000000000) > 0 then
+    Result := Result or ($FFFFFFFFFFFFFFFF shl (64 - ShiftBits));
+
+  /// ++++++ Alternative Variant ++++++ ///
+
+  // Result := (Value shr ShiftBits) or ((0 - ((Value shr 63) and 1)) shl
+  // (64 - ShiftBits));
 {$ENDIF FPC}
 end;
 
@@ -170,11 +180,11 @@ begin
   System.Assert(a_n >= 0);
 {$ENDIF DEBUG}
 {$IFDEF FPC}
-  result := RolDWord(a_value, a_n);
+  Result := RolDWord(a_value, a_n);
 {$ELSE}
   a_n := a_n and 31;
 
-  result := (a_value shl a_n) or (a_value shr (32 - a_n));
+  Result := (a_value shl a_n) or (a_value shr (32 - a_n));
 {$ENDIF FPC}
 end;
 
@@ -184,11 +194,11 @@ begin
   System.Assert(a_n >= 0);
 {$ENDIF DEBUG}
 {$IFDEF FPC}
-  result := RolQWord(a_value, a_n);
+  Result := RolQWord(a_value, a_n);
 {$ELSE}
   a_n := a_n and 63;
 
-  result := (a_value shl a_n) or (a_value shr (64 - a_n));
+  Result := (a_value shl a_n) or (a_value shr (64 - a_n));
 {$ENDIF FPC}
 end;
 
@@ -198,11 +208,11 @@ begin
   System.Assert(a_n >= 0);
 {$ENDIF DEBUG}
 {$IFDEF FPC}
-  result := RorDWord(a_value, a_n);
+  Result := RorDWord(a_value, a_n);
 {$ELSE}
   a_n := a_n and 31;
 
-  result := (a_value shr a_n) or (a_value shl (32 - a_n));
+  Result := (a_value shr a_n) or (a_value shl (32 - a_n));
 {$ENDIF FPC}
 end;
 
@@ -212,11 +222,11 @@ begin
   System.Assert(a_n >= 0);
 {$ENDIF DEBUG}
 {$IFDEF FPC}
-  result := RorQWord(a_value, a_n);
+  Result := RorQWord(a_value, a_n);
 {$ELSE}
   a_n := a_n and 63;
 
-  result := (a_value shr a_n) or (a_value shl (64 - a_n));
+  Result := (a_value shr a_n) or (a_value shl (64 - a_n));
 {$ENDIF FPC}
 end;
 
