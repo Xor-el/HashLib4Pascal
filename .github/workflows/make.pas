336c872bf0b222655bf3950937e0efc1e3058a63
Artem V. Ageev <artem.v.ageev@gmail.com>
2025-01-14 10:09:06 +0200

    add MSWINDOWS support

--- .github/workflows/make.pas
+++ .github/workflows/make.pas
@@ -91,6 +91,7 @@ begin
       List := FindAllFiles(PackagePath, '*.lpk', True);
       try
         for Item in List do
+        try
           if RunCommand('lazbuild', ['--add-package-link', Item], Output) then
             Writeln(#27'[33m', 'added ', Item, #27'[0m')
           else
@@ -98,6 +99,10 @@ begin
             ExitCode += 1;
             Writeln(#27'[31m', 'added ', Item, #27'[0m');
           end;
+        except
+          on E: Exception do
+            WriteLn('Error: ' + E.ClassName + #13#10 + E.Message);
+        end;
       finally
         List.Free;
       end;
@@ -108,22 +113,33 @@ begin
     for Each in List do
     begin
       Writeln(#27'[33m', 'build ', Each, #27'[0m');
-      if RunCommand('lazbuild', ['--build-all', '--recursive',
-        '--no-write-project', Each], Output) then
-        for Line in SplitString(Output, LineEnding) do
-        begin
-          if Pos('Linking', Line) <> 0 then
+      try
+        if RunCommand('lazbuild', ['--build-all', '--recursive',
+          '--no-write-project', Each], Output) then
+          for Line in SplitString(Output, LineEnding) do
           begin
-            if not RunCommand({$IFDEF MSWINDOWS}'&'{$ELSE}'command'{$ENDIF}, [SplitString(Line, ' ')[2],
-              '--all', '--format=plain', '--progress'], Output) then
-              ExitCode += 1;
-            WriteLn(Output);
-          end;
-        end
-      else
-        for Line in SplitString(Output, LineEnding) do
-          if Pos('Fatal', Line) <> 0 or Pos('Error', Line) then
-            Writeln(#27'[31m', Line, #27'[0m');
+            if Pos('Linking', Line) <> 0 then
+            begin
+              if not RunCommand(
+                {$IFDEF MSWINDOWS}
+                '&'
+                {$ELSE}
+                'command'
+                {$ENDIF}
+                , [SplitString(Line, ' ')[2], '--all', '--format=plain', '--progress'],
+                Output) then
+                ExitCode += 1;
+              WriteLn(Output);
+            end;
+          end
+        else
+          for Line in SplitString(Output, LineEnding) do
+            if Pos('Fatal', Line) <> 0 or Pos('Error', Line) then
+              Writeln(#27'[31m', Line, #27'[0m');
+      except
+        on E: Exception do
+          WriteLn('Error: ' + E.ClassName + #13#10 + E.Message);
+      end;
     end;
   finally
     List.Free;
